58a828aff1a1a3c48e5734ab7a13062e73846fa3
amruta <hail.radical@gmail.com>
2018-09-25 20:50:38 -0600

    added args for hue change

--- utils/modifyhue.py
+++ utils/modifyhue.py
@@ -22,14 +22,12 @@ def change_hue(folder_path, sub_dir_list, deg=10):
 		hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 		hsv_img[:, :, 1] = hsv_img[:, :, 1] + deg
 		new_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
-		cv2.imwrite(i, new_img)
+		cv2.imwrite(img_path, new_img)
 
 
 if __name__ == '__main__':
-	path = r"C:\Users\Amruta\Desktop\Project\Datasets\CamVid\test"
-	# input("Give the location of image collection")
-	# degree = int(input("Give the degree of change"))
-	degree = 50
+	path = input("Give the location of image collection: ")
+	degree = int(input("Give the degree of change: "))
 	sub_dirs = get_immediate_subdirectories(path)
 	change_hue(path, sub_dirs, degree)
 
