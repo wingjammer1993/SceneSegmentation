0770968072899c5e5621e4c2474ff0ee85f0583c
amruta <hail.radical@gmail.com>
2018-10-07 12:59:29 -0600

    Deeplab + PVOC

---
+++ DeepLab_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# === 2. List of SBATCH arguements ===
+#SBATCH --nodes=4
+#SBATCH --time=00:59:00
+#SBATCH --qos=normal
+#SBATCH --partition=sgpu
+#SBATCH --ntasks=1
+#SBATCH --job-name=enet-hue-job
+#SBATCH --output=deeplab-hue-job.%j.out
+
+# === 3. Purge and load needed modules ===
+module purge
+module load intel/17.4
+module load python/3.5.1
+
+# === 4. Additional commands needed to run a program ===
+source ./semseg/bin/activate
+
+cd PyTorch-Deeplab-master
+# === 5. Running the program ===
+python ./evaluate.py --dataset-dir /projects/amra8468/VOCdevkit/VOC2012 --restore-from /projects/amra8468/VOC12_scenes_20000.pth
+
+
+
