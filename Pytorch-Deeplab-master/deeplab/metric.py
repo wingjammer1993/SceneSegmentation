29fb1ef520416f66c94a51ba21ddc0bb0e6dfb47
amruta <hail.radical@gmail.com>
2018-10-07 14:58:38 -0600

    Deeplab + PVOC

--- Pytorch-Deeplab-master/deeplab/metric.py
+++ Pytorch-Deeplab-master/deeplab/metric.py
@@ -1,16 +1,19 @@
 import os, sys
 import numpy as np
-
+import cv2
 from multiprocessing import Pool 
-import copy_reg
+import copyreg as copy_reg
 import types
+
 def _pickle_method(m):
     if m.im_self is None:
         return getattr, (m.im_class, m.im_func.func_name)
     else:
         return getattr, (m.im_self, m.im_func.func_name)
 
+
 copy_reg.pickle(types.MethodType, _pickle_method)
+
 
 class ConfusionMatrix(object):
 
