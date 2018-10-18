4405e22e53fe94ccca66e97a4191041adda9ce0e
amruta <hail.radical@gmail.com>
2018-10-17 18:37:25 -0600

    Added script for variations

--- semseg_val.sh
+++ semseg_val.sh
@@ -20,62 +20,62 @@ source ./semseg/bin/activate
 cd pytorch-semseg-master
 
 # === 5. Running the program ===
-python ./validate.py --config ./configs/$1 -hue -0.5
-python ./validate.py --config ./configs/$1 -hue -0.4
-python ./validate.py --config ./configs/$1 -hue -0.3
-python ./validate.py --config ./configs/$1 -hue -0.2
-python ./validate.py --config ./configs/$1 -hue -0.1
+python ./validate.py --config ./configs/$1 --hue -0.5
+python ./validate.py --config ./configs/$1 --hue -0.4
+python ./validate.py --config ./configs/$1 --hue -0.3
+python ./validate.py --config ./configs/$1 --hue -0.2
+python ./validate.py --config ./configs/$1 --hue -0.1
 python ./validate.py --config ./configs/$1
-python ./validate.py --config ./configs/$1 -hue 0.1
-python ./validate.py --config ./configs/$1 -hue 0.2
-python ./validate.py --config ./configs/$1 -hue 0.3
-python ./validate.py --config ./configs/$1 -hue 0.4
-python ./validate.py --config ./configs/$1 -hue 0.5
+python ./validate.py --config ./configs/$1 --hue 0.1
+python ./validate.py --config ./configs/$1 --hue 0.2
+python ./validate.py --config ./configs/$1 --hue 0.3
+python ./validate.py --config ./configs/$1 --hue 0.4
+python ./validate.py --config ./configs/$1 --hue 0.5
 
-python ./validate.py --config ./configs/$1 -contrast 0
-python ./validate.py --config ./configs/$1 -contrast 0.2
-python ./validate.py --config ./configs/$1 -contrast 0.4
-python ./validate.py --config ./configs/$1 -contrast 0.6
-python ./validate.py --config ./configs/$1 -contrast 0.8
+python ./validate.py --config ./configs/$1 --contrast 0
+python ./validate.py --config ./configs/$1 --contrast 0.2
+python ./validate.py --config ./configs/$1 --contrast 0.4
+python ./validate.py --config ./configs/$1 --contrast 0.6
+python ./validate.py --config ./configs/$1 --contrast 0.8
 python ./validate.py --config ./configs/$1
-python ./validate.py --config ./configs/$1 -contrast 1.2
-python ./validate.py --config ./configs/$1 -contrast 1.4
-python ./validate.py --config ./configs/$1 -contrast 1.6
-python ./validate.py --config ./configs/$1 -contrast 1.8
-python ./validate.py --config ./configs/$1 -contrast 2.0
+python ./validate.py --config ./configs/$1 --contrast 1.2
+python ./validate.py --config ./configs/$1 --contrast 1.4
+python ./validate.py --config ./configs/$1 --contrast 1.6
+python ./validate.py --config ./configs/$1 --contrast 1.8
+python ./validate.py --config ./configs/$1 --contrast 2.0
 
-python ./validate.py --config ./configs/$1 -gamma 0
-python ./validate.py --config ./configs/$1 -gamma 0.2
-python ./validate.py --config ./configs/$1 -gamma 0.4
-python ./validate.py --config ./configs/$1 -gamma 0.6
-python ./validate.py --config ./configs/$1 -gamma 0.8
+python ./validate.py --config ./configs/$1 --gamma 0
+python ./validate.py --config ./configs/$1 --gamma 0.2
+python ./validate.py --config ./configs/$1 --gamma 0.4
+python ./validate.py --config ./configs/$1 --gamma 0.6
+python ./validate.py --config ./configs/$1 --gamma 0.8
 python ./validate.py --config ./configs/$1
-python ./validate.py --config ./configs/$1 -gamma 1.2
-python ./validate.py --config ./configs/$1 -gamma 1.4
-python ./validate.py --config ./configs/$1 -gamma 1.6
-python ./validate.py --config ./configs/$1 -gamma 1.8
-python ./validate.py --config ./configs/$1 -gamma 2.0
+python ./validate.py --config ./configs/$1 --gamma 1.2
+python ./validate.py --config ./configs/$1 --gamma 1.4
+python ./validate.py --config ./configs/$1 --gamma 1.6
+python ./validate.py --config ./configs/$1 --gamma 1.8
+python ./validate.py --config ./configs/$1 --gamma 2.0
 
-python ./validate.py --config ./configs/$1 -saturation 0
-python ./validate.py --config ./configs/$1 -saturation 0.2
-python ./validate.py --config ./configs/$1 -saturation 0.4
-python ./validate.py --config ./configs/$1 -saturation 0.6
-python ./validate.py --config ./configs/$1 -saturation 0.8
+python ./validate.py --config ./configs/$1 --saturation 0
+python ./validate.py --config ./configs/$1 --saturation 0.2
+python ./validate.py --config ./configs/$1 --saturation 0.4
+python ./validate.py --config ./configs/$1 --saturation 0.6
+python ./validate.py --config ./configs/$1 --saturation 0.8
 python ./validate.py --config ./configs/$1
-python ./validate.py --config ./configs/$1 -saturation 1.2
-python ./validate.py --config ./configs/$1 -saturation 1.4
-python ./validate.py --config ./configs/$1 -saturation 1.6
-python ./validate.py --config ./configs/$1 -saturation 1.8
-python ./validate.py --config ./configs/$1 -saturation 2.0
+python ./validate.py --config ./configs/$1 --saturation 1.2
+python ./validate.py --config ./configs/$1 --saturation 1.4
+python ./validate.py --config ./configs/$1 --saturation 1.6
+python ./validate.py --config ./configs/$1 --saturation 1.8
+python ./validate.py --config ./configs/$1 --saturation 2.0
 
-python ./validate.py --config ./configs/$1 -brightness 0
-python ./validate.py --config ./configs/$1 -brightness 0.2
-python ./validate.py --config ./configs/$1 -brightness 0.4
-python ./validate.py --config ./configs/$1 -brightness 0.6
-python ./validate.py --config ./configs/$1 -brightness 0.8
+python ./validate.py --config ./configs/$1 --brightness 0
+python ./validate.py --config ./configs/$1 --brightness 0.2
+python ./validate.py --config ./configs/$1 --brightness 0.4
+python ./validate.py --config ./configs/$1 --brightness 0.6
+python ./validate.py --config ./configs/$1 --brightness 0.8
 python ./validate.py --config ./configs/$1
-python ./validate.py --config ./configs/$1 -brightness 1.2
-python ./validate.py --config ./configs/$1 -brightness 1.4
-python ./validate.py --config ./configs/$1 -brightness 1.6
-python ./validate.py --config ./configs/$1 -brightness 1.8
-python ./validate.py --config ./configs/$1 -brightness 2.0
+python ./validate.py --config ./configs/$1 --brightness 1.2
+python ./validate.py --config ./configs/$1 --brightness 1.4
+python ./validate.py --config ./configs/$1 --brightness 1.6
+python ./validate.py --config ./configs/$1 --brightness 1.8
+python ./validate.py --config ./configs/$1 --brightness 2.0
