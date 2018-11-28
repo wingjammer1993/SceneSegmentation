ef0914c7434b31a1cd38c510e1c67b65ba101427
amruta <hail.radical@gmail.com>
2018-11-27 18:27:33 -0700

    map script

---
+++ map_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# === 2. List of SBATCH arguements ===
+#SBATCH --nodes=4
+#SBATCH --time=20:00:00
+#SBATCH --qos=normal
+#SBATCH --partition=sgpu
+#SBATCH --ntasks=1
+#SBATCH --job-name=map-job
+#SBATCH --output=map-job.%j.out
+
+# === 3. Purge and load needed modules ===
+module purge
+module load intel/17.4
+module load python/3.5.1
+
+# === 4. Additional commands needed to run a program ===
+source ./semseg/bin/activate
+
+cd utils
+
+# === 5. Running the program ===
+python ./mapillary_val_gen.py
